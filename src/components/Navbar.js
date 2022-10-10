7fd169d43dd5d7e965a175e29f8cb92887b105a8
camara92 <88945535+camara92@users.noreply.github.com>
2022-10-11 00:51:14 +0200

    navigation entre les pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,53 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { useState , useEffect} from 'react';
 
 const Header=()=> {
+  const [activeLink, SetactiveLink] = useState('home'); 
+  const [scrolled, setScrolled] = useState(false); 
+  useEffect(
+    ()=>{
+      const onScroll = () =>{
+        if(window.scrollY> 50){
+          setScrolled(true);
+        }else{
+          setScrolled(false); 
+        }
+      }
+      window.addEventListener("scroll", onscroll); 
+      return ()=> window.removeEventListener("scroll", onScroll); 
+
+    }
+  , [])
+  const onUpdateActiveLink = (value)=>{
+    SetactiveLink(value)
+  }
   return (
-    <Navbar className="light header-page" expand="lg">
+    <Navbar className={scrolled ? "scrolled" : ""} expand="lg">
       <Container>
-        <Navbar.Brand href="#home">
-            <img className='rounded' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRb0lOhuAbflByagOfYUg5R8F1zGO1aN4woA&usqp=CAU' alt='logo_du_site'  BorderRadius ={30} width={30}/>
+        <Navbar.Brand href="#home" >
+            <img className='rounded image' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRb0lOhuAbflByagOfYUg5R8F1zGO1aN4woA&usqp=CAU' alt='logo_du_site'/>
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <span className='navbar-toggler-icon'></span>
+          </Navbar.Toggle> 
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Accueil</Nav.Link>
-            <Nav.Link href="#link">Qui suis-je </Nav.Link>
-            <NavDropdown title="Daouda" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Projets</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Compétences 
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3"> Passions dev  </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="#home" className={activeLink ==='home' ? 'active navbar-link' : 'navbar-link'} onClick={()=> onUpdateActiveLink('home')}>Home</Nav.Link>
+            <Nav.Link href="#skills" className={activeLink ==='skills' ? 'active navbar-link' : 'navbar-link'} onClick={()=> onUpdateActiveLink('skills')}>Skills</Nav.Link>
+            <Nav.Link href="#projects" className={activeLink ==='projects' ? 'active navbar-link' : 'navbar-link' } onClick={()=> onUpdateActiveLink('projects')}>Projects </Nav.Link>
           </Nav>
+          <span className='navbar-text'>
+            <div className='social-icon'>
+              <a href="#"><img src={''} alt='' /></a>
+              <a href="#"><img src={''} alt='' /></a>
+              <a href="#"><img src={''} alt='' /></a>
+            </div>
+            <button className='vvd' onClick={()=> console.log('Daouda ! ')}>
+              <span>Let's connect </span>
+            </button>
+          </span>
         </Navbar.Collapse>
       </Container>
     </Navbar>
